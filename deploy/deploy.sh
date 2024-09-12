9b5f4947bd7a8cf77632ca3ac9b74fd7a5826a38
artsdeep <artsdeep@gmail.com>
2024-09-12 23:13:04 +0300

    changed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,9 +4,8 @@
 kubectl apply -f postgres-deployment.yaml
 echo "PostgreSQL deployment applied."
 
-# Применяем конфигурации для Kafka
-kubectl apply -f kafka-deployment.yaml
-echo "Kafka deployment applied."
+kubectl apply -f java-app-monolith-deployment.yaml
+echo "Java app Thing Manager deployment applied."
 
 kubectl apply -f java-app-telemetry-deployment.yaml
 echo "Java app Thing Manager deployment applied."
@@ -14,5 +13,4 @@ echo "Java app Thing Manager deployment applied."
 kubectl apply -f java-app-thing-manager-deployment.yaml
 echo "Java app Thing Mnager deployment applied."
 
-kubectl apply -f java-app-thing-manager-deployment.yaml
-echo "Java app Thing Manager deployment applied."
+
